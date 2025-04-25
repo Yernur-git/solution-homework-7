dcca32cae4981045e6f716039b02fa08b6f835b4
Yernur <erkeerke349@gmail.com>
2025-04-25 22:32:59 +0500

    SeasonIterator realisation

--- part-1/SeasonIterator.java
+++ part-1/SeasonIterator.java
@@ -1,2 +1,21 @@
-public class SeasonIterator {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeasonIterator implements EpisodeIterator {
+    private List<Episode> episodes = new ArrayList<>();
+    private int currentIndex = 0;
+
+    public SeasonIterator(List<Episode> episodes) {
+        this.episodes = episodes;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < episodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        return episodes.get(currentIndex++);
+    }
 }
