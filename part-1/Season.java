8e4a25fd394152801054e184e41123561ef5232e
Yernur <erkeerke349@gmail.com>
2025-04-26 12:41:45 +0500

    Season class with iterator

--- part-1/Season.java
+++ part-1/Season.java
@@ -1,2 +1,52 @@
-public class Season {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Season implements Iterable<Episode> {
+    private List<Episode> episodes = new ArrayList<>();
+
+    public Season() {
+        this.episodes = new ArrayList<>();
+    }
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public int episodeCount() {
+        return episodes.size();
+    }
+
+    public Episode getEpisode(int index) {
+        return episodes.get(index);
+    }
+
+    public EpisodeIterator normalIterator() {
+        return new SeasonIterator(episodes);
+    }
+
+    public EpisodeIterator reverseIterator() {
+        return new ReverseSeasonIterator(episodes);
+    }
+
+    public EpisodeIterator shuffleIterator() {
+        return new ShuffleSeasonIterator(episodes);
+    }
+
+    @Override
+    public Iterator<Episode> iterator() {
+        return new Iterator<Episode>() {
+            private EpisodeIterator it = normalIterator();
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public Episode next() {
+                return it.next();
+            }
+        };
+    }
 }
