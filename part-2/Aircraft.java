a9a14f25d87e5dfd7fc59bbf5956c825e5855a9e
Yernur <erkeerke349@gmail.com>
2025-04-26 16:42:27 +0500

    add Aircraft

--- part-2/Aircraft.java
+++ part-2/Aircraft.java
@@ -1,2 +1,17 @@
-public class Aircraft {
+abstract class Aircraft {
+    protected String id;
+    protected int fuelLevel;
+
+    public Aircraft(String id, int fuelLevel) {
+        this.id = id;
+        this.fuelLevel = fuelLevel;
+    }
+
+    public abstract void receive(String msg);
+
+    public void send(String msg, TowerMediator m) { m.broadcast(msg, this);}
+
+    public String getId() {return id;}
+
+    public int getFuelLevel() {return fuelLevel;}
 }
