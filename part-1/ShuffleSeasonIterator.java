5597dfb87dd524f21e93bd632c3234d139a3a4e0
Yernur <erkeerke349@gmail.com>
2025-04-26 09:36:52 +0500

    add ShuffleSeasonIterator

--- part-1/ShuffleSeasonIterator.java
+++ part-1/ShuffleSeasonIterator.java
@@ -1,2 +1,24 @@
-public class ShuffleSeasonIterator {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private int currentIndex = 0;
+    private List<Episode> shuffledEpisodes;
+
+    public ShuffleSeasonIterator(List<Episode> episodes) {
+        this.shuffledEpisodes = new ArrayList<>(episodes);
+        Collections.shuffle(shuffledEpisodes, new Random(42));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < shuffledEpisodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        return shuffledEpisodes.get(currentIndex++);
+    }
 }
