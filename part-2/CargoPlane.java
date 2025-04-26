f28cccb96dfe2429ebc141a94724ee2f368720e7
Yernur <erkeerke349@gmail.com>
2025-04-26 17:44:04 +0500

    add CargoPlane

--- part-2/CargoPlane.java
+++ part-2/CargoPlane.java
@@ -1,2 +1,10 @@
-public class CargoPlane {
+public class CargoPlane extends Aircraft {
+    public CargoPlane(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("[CargoPlane " + id + "]: " + msg);
+    }
 }
