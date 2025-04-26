9837187f92043c02e4865d7ed34ec072b8e79c2f
Yernur <erkeerke349@gmail.com>
2025-04-26 17:47:02 +0500

    add Helicopter

--- part-2/Helicopter.java
+++ part-2/Helicopter.java
@@ -1,2 +1,10 @@
-public class Helicopter {
+public class Helicopter extends Aircraft {
+    public Helicopter(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("[Helicopter " + id + "]: " + msg);
+    }
 }
