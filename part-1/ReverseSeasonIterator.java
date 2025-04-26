c91f2dd895e99da9a67d397ed2064d64631de727
Yernur <erkeerke349@gmail.com>
2025-04-26 09:24:31 +0500

    add ReverseSeasonIterator

--- part-1/ReverseSeasonIterator.java
+++ part-1/ReverseSeasonIterator.java
@@ -1,2 +1,21 @@
-public class ReverseSeasonIterator {
+import java.util.List;
+
+public class ReverseSeasonIterator implements EpisodeIterator {
+    private List<Episode> episodes;
+    private int currentIndex;
+
+    public ReverseSeasonIterator(List<Episode> episodes) {
+        this.episodes = episodes;
+        this.currentIndex = episodes.size() - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex >= 0;
+    }
+
+    @Override
+    public Episode next() {
+        return episodes.get(currentIndex--);
+    }
 }
