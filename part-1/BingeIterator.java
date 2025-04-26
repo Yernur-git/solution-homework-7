58ee0c51997a3aa2e1f6415194f09b314e618bb5
Yernur <erkeerke349@gmail.com>
2025-04-26 13:04:52 +0500

    add BingeIterator

--- part-1/BingeIterator.java
+++ part-1/BingeIterator.java
@@ -1,2 +1,39 @@
-public class BingeIterator {
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class BingeIterator implements EpisodeIterator {
+    private List<Season> seasons;
+    private int currentSeason = 0;
+    private EpisodeIterator currentIterator = null;
+
+    public BingeIterator(List<Season> seasons) {
+        this.seasons = seasons;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (currentIterator != null && currentIterator.hasNext()) {
+            return true;
+        }
+
+        while (currentSeason < seasons.size()) {
+            Season season = seasons.get(currentSeason);
+            if (season.episodeCount() > 0) {
+                currentIterator = season.normalIterator();
+                currentSeason++;
+                return true;
+            }
+            currentSeason++;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Episode next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return currentIterator.next();
+    }
 }
